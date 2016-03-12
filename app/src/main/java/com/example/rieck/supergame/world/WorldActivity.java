eb002e115240e1436092cf76b08e3851aa685f2b
rieck <strie103@uni-duesseldorf.de>
2016-03-12 13:46:14 +0100

    Sprite bewegen

--- app/src/main/java/com/example/rieck/supergame/world/WorldActivity.java
+++ app/src/main/java/com/example/rieck/supergame/world/WorldActivity.java
@@ -1,8 +1,10 @@
 package com.example.rieck.supergame.world;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WorldActivity extends AppCompatActivity {
 
@@ -10,9 +12,9 @@ public class WorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         WorldView view = new WorldView(this);
         view.setBackgroundColor(Color.BLACK);
         setContentView(view);
+
     }
 }
--- app/src/main/java/com/example/rieck/supergame/world/WorldView.java
+++ app/src/main/java/com/example/rieck/supergame/world/WorldView.java
@@ -2,7 +2,11 @@ package com.example.rieck.supergame.world;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -11,33 +15,59 @@ import android.view.SurfaceView;
 public class WorldView extends SurfaceView {
 
     Paint paint = new Paint();
+    int x = 0;
+    int xSpeed= 0;
+    Character hero;
 
     public WorldView(Context context)  {
         super(context);
+        hero = new Character(getResources());
+        final GameLoopThread thread = new GameLoopThread(this);
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback(){
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                thread.setRunning(true);
+                thread.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                boolean retry = true;
+                thread.setRunning(false);
+                while (retry) {
+                    try {
+                        thread.join();
+                        retry = false;
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        Character hero = new Character(getResources(), canvas);
-        hero.draw();
         hero.moveDown();
-        hero.moveDown();
-        hero.moveUp();
+        hero.draw(canvas);
+    }
 
-        /*paint.setColor(Color.CYAN);
-        for (int y=1; y <= 10; y++) {
-            if (y % 2 == 0)
-                paint.setColor(Color.BLUE);
-            else
-                paint.setColor(Color.YELLOW);
-            for (int x=1; x <= 10; x++) {
-                canvas.drawRect(10*x, 10*y, 10*x + 10, 10*y + 10, paint);
-            }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.e("TOUCH", "TOUCH");
+        //hero.moveDown();
+
+        /*if (event.getY() > hero.getYPos()) {
+            hero.moveDown();
+            //hero.draw();
         }
-
-        canvas.drawRect(150, 150, 151, 151, paint);*/
-
-
+        else hero.moveUp();*/
+        return true;
     }
 }
